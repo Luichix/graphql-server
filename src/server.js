8f4737ec526a893cc9c4d68f19a1b04231be3c74
Luichix <luigui.rex@gmail.com>
2020-12-15 00:09:25 -0600

    Review server

--- src/server.js
+++ src/server.js
@@ -30,8 +30,6 @@ app.use (express.json ());
 app.use (passport.initialize());
 app.use (passport.session());
 app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
 
 // Global Variables
 app.use ((req,res,next)=> {
