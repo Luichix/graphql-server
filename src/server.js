f2927444405fd3c20e53755c6467f8e7692a587c
Luichix <luigui.rex@gmail.com>
2022-02-23 19:08:13 -0600

    configurando midleware

--- src/server.js
+++ src/server.js
@@ -18,12 +18,23 @@ let notes = [
       important: false
     },
     {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2019-05-30T19:20:14.298Z",
+        important: true
     }
 ]
+
+const requestLogger = (request, response, next) =>{
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(requestLogger)
+
 
 app.get('/', (request,response) => {
     response.send('<h1>Hello World</h1>')
@@ -53,8 +64,8 @@ app.delete('/api/notes/:id', (request,response)=> {
 
 const generateId = () => {  
     const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
     return maxId + 1
 }
 
@@ -66,29 +77,24 @@ app.post('/api/notes', (request, response) => {
             error: 'content missing'
         })
     }
-
+    
     const note ={
         content: body.content,
         important: body.important || false,
         date: new Date(),
         id: generateId()
     }
-
+    
     notes = notes.concat(note)
-
+    
     response.json(note)
 })
 
-
-const requestLogger = (request, response, next) =>{
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
+const unknnownEndpoint = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'})
 }
 
-app.use(requestLogger)
+app.use(unknnownEndpoint)
 
 const PORT = 3001
 app.listen (PORT, () => {
