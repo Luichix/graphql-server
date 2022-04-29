7827af41e2a2f3bcf26e81d697e48c6f923ed20e
Luichix <luichix.rex@gmail.com>
2022-04-28 21:27:51 -0600

    add db json

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const cors = require('cors')
 let notes = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
+    content: 'HTML is easy',
+    date: '2022-01-10T17:30:31.098Z',
     important: true
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
+    content: 'Browser can execute only Javascript',
+    date: '2022-01-10T18:39:34.091Z',
     important: false
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2022-01-10T19:20:14.298Z',
     important: true
   }
 ]
@@ -54,8 +54,8 @@ app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
+    return response.status(400).json({
+      error: 'content missing'
     })
   }
 
@@ -63,7 +63,7 @@ app.post('/api/notes', (request, response) => {
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
+    id: generateId()
   }
 
   notes = notes.concat(note)
