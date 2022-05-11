791775d31e91d521768b4d5f7ad4114c9ec8eeb4
Luichix <luichix.rex@gmail.com>
2022-05-11 14:15:50 -0600

    add mutation addUser

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const persons = [
     country: 'US',
     city: 'Chinandega',
     address: 'Hotel Glomar 3 1/2 al sur',
-    company: 'ChessLand'
-  }
+    company: 'ChessLand',
+  },
 ]
 
 const typeDefs = gql`
@@ -32,6 +32,11 @@ const typeDefs = gql`
     country: String
     id: String
   }
+
+  type AddPerson {
+    mid: String
+  }
+
   type UpdatePerson {
     modified: String
   }
@@ -40,95 +45,85 @@ const typeDefs = gql`
     personCount: Int!
     allPersons: [Person!]!
     findPerson(name: String!): Person
-    userInfo(uid: String): Person 
+    userInfo(uid: String): Person
   }
   type Mutation {
+    addUser(
+      uid: String!
+      name: String
+      email: String!
+      phoneNumber: String
+      industry: String
+      employeeCount: String
+      country: String
+      city: String
+      address: String
+      company: String
+    ): AddPerson
+
     updateUser(
-      uid: String!, 
-      name: String, 
-      email: String, 
-      phoneNumber: String, 
-      industry: String, 
-      employeeCount: String, 
-      country: String, 
-      city: String, 
-      address: String, 
-      company: String): UpdatePerson
-    editBasic(
-      uid: String!, 
-      name: String!, 
-      email: String!,
-      company: String!, 
-      phoneNumber: String!): Person
-     editAddress(
-      uid: String!, 
-      country: String!, 
-      city: String!,
-      address: String!): Person
-    }
+      uid: String!
+      name: String
+      email: String
+      phoneNumber: String
+      industry: String
+      employeeCount: String
+      country: String
+      city: String
+      address: String
+      company: String
+    ): UpdatePerson
+  }
 `
 
 const resolvers = {
   Query: {
     personCount: () => persons.length,
     allPersons: () => persons,
-    findPerson: (root, args) =>
-      persons.find(p => p.name === args.name),
-    userInfo: (root, args) =>
-      persons.find(p => p.uid === args.uid)
+    findPerson: (root, args) => persons.find((p) => p.name === args.name),
+    userInfo: (root, args) => persons.find((p) => p.uid === args.uid),
   },
   Mutation: {
+    addUser: (root, args) => {
+      const person = {
+        uid: args.uid,
+        name: args.name,
+        email: args.email,
+        phoneNumber: args.phoneNumber,
+        industry: args.industry,
+        employeeCount: args.employeeCount,
+        country: args.country,
+        city: args.city,
+        address: args.address,
+        company: args.company,
+      }
+      persons.push(person)
+      return { mid: 'register' }
+    },
     updateUser: (root, args) => {
-      const personIndex = persons.findIndex(p => p.uid === args.uid)
+      const personIndex = persons.findIndex((p) => p.uid === args.uid)
       if (personIndex === -1) return { modified: 'false' }
       const person = persons[personIndex]
       const newPerson = { ...person, ...args }
       persons[personIndex] = newPerson
       return { modified: 'modified' }
     },
-    editBasic: (root, args) => {
-      const personIndex = persons.findIndex(p => p.uid === args.uid)
-      if (personIndex === -1) return null
-      const person = persons[personIndex]
-      const updatePerson = {
-        ...person,
-        name: args.name,
-        email: args.email,
-        company: args.company,
-        phoneNumber: args.phoneNumber
-      }
-      persons[personIndex] = updatePerson
-      return updatePerson
-    },
-    editAddress: (root, args) => {
-      const personIndex = persons.findIndex(p => p.uid === args.uid)
-      if (personIndex === -1) return null
-      const person = persons[personIndex]
-      const updatePerson = {
-        ...person,
-        country: args.country,
-        city: args.city,
-        address: args.address
-      }
-      persons[personIndex] = updatePerson
-      return updatePerson
-    }
-  }
+  },
 }
-async function startApolloServer () {
+async function startApolloServer() {
   const app = express()
-  const httpServer = (app)
+  const httpServer = app
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   })
 
   await server.start()
 
   // Mount Apollo middleware here.
   server.applyMiddleware({ app, path: '/graphql', cors: true })
-  await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve))
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve))
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
   return { server, app }
 }
