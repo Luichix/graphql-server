2afd52cb1a74ce1126bd2b20b508b61a6c00f571
Luichix <luichix.rex@gmail.com>
2022-06-13 13:55:45 -0600

    add subscription

--- src/yoga.js
+++ src/yoga.js
@@ -1,10 +1,17 @@
-const { createServer } = require('@graphql-yoga/node')
+const { createServer } = require('http')
+const { ApolloServer, gql } = require('apollo-server-express')
+const express = require('express')
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
+const { WebSocketServer } = require('ws')
+const { useServer } = require('graphql-ws/lib/use/ws')
+const { PubSub } = require('graphql-subscriptions')
 
 const messages = [
   {
     _id: 6,
     text: 'Unicamente, comenteme la hora de entrega por favor.',
-    createdAt: new Date(),
+    createdAt: '2022-06-08T03:45:51.720Z',
     user: {
       _id: 1,
       avatar: 'https://i.pravatar.cc/300',
@@ -12,25 +19,8 @@ const messages = [
   },
   {
     _id: '4477f03f-4a6f-b2a9-73447345711a',
-    createdAt: '2022-06-08T03:45:51.720Z',
     text: 'En un momento, le confirmo la hora de entrega',
-    quickReplies: {
-      type: 'radio', // or 'checkbox',
-      keepIt: true,
-      values: [
-        {
-          title: '😋 Yes',
-          value: 'yes',
-        },
-        {
-          title: '😞 Nope. What?',
-          value: 'no',
-        },
-      ],
-    },
-    sent: true,
-    received: true,
-    pending: true,
+    createdAt: '2022-06-08T03:45:51.720Z',
     user: {
       _id: 'luigui.rex@gmail.com',
       avatar: 'https://i.pravatar.cc/300',
@@ -38,11 +28,8 @@ const messages = [
   },
   {
     _id: '4477f03f-6e05-4a6f-b2a9-73447345711a',
-    createdAt: '2022-06-08T03:45:51.720Z',
     text: 'Esta bien, no hay ningun problema, unicamente me comparte su nombre y numero de telefono.',
-    sent: true,
-    received: true,
-    pending: true,
+    createdAt: '2022-06-08T03:45:51.720Z',
     user: {
       _id: 'luigui.rex@gmail.com',
       avatar: 'https://i.pravatar.cc/300',
@@ -50,11 +37,8 @@ const messages = [
   },
   {
     _id: '4477f03f-6e05-4a6f-b2a9',
-    createdAt: '2022-06-08T03:45:51.720Z',
     text: 'Buenos dias, si tenemos queso palmito en estos momentos.',
-    sent: true,
-    received: true,
-    pending: true,
+    createdAt: '2022-06-08T03:45:51.720Z',
     user: {
       _id: 'luigui.rex@gmail.com',
       avatar: 'https://i.pravatar.cc/300',
@@ -64,9 +48,6 @@ const messages = [
     _id: 5,
     text: 'Unicamente, comenteme la hora de entrega por favor.',
     createdAt: '2022-06-08T03:45:51.720Z',
-    sent: true,
-    received: true,
-    pending: true,
     user: {
       _id: 1,
       avatar: 'https://i.pravatar.cc/300',
@@ -78,7 +59,6 @@ const messages = [
     createdAt: '2022-06-08T03:45:51.720Z',
     user: {
       _id: 1,
-      name: 'Client One',
       avatar: 'https://placeimg.com/140/140/any',
     },
   },
@@ -88,7 +68,6 @@ const messages = [
     createdAt: '2022-06-08T03:45:51.720Z',
     user: {
       _id: 1,
-      name: 'Client One',
       avatar: 'https://placeimg.com/140/140/any',
     },
   },
@@ -98,7 +77,6 @@ const messages = [
     createdAt: '2022-06-08T03:45:51.720Z',
     user: {
       _id: 1,
-      name: 'Client One',
       avatar: 'https://placeimg.com/140/140/any',
     },
   },
@@ -108,51 +86,114 @@ const messages = [
     createdAt: '2022-06-08T03:45:51.720Z',
     user: {
       _id: 1,
-      name: 'Client One',
       avatar: 'https://placeimg.com/140/140/any',
     },
   },
 ]
 
-const typeDefs = `
+const pubsub = new PubSub()
 
-type User {
-  _id: ID!
-  name: String
-  avatar: String
-}
-type Message {
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    name: String
+    avatar: String
+  }
+  type Message {
     _id: ID!
     text: String!
     createdAt: String!
-    user: User! 
-}
-
-type Query {
-  messages: [Message!]
-}
+    user: User!
+  }
+  type Query {
+    messages: [Message!]
+  }
+  type Mutation {
+    postMessage(text: String!, createdAt: String!): ID!
+  }
+  type Subscription {
+    messages: [Message!]
+  }
 `
-// type Mutation {
-//     postMessage(user: String!, text: String!): Message: ID!
-// }
+
+// const subscribers = []
+// const onMessagesUpdates = (fn) => subscribers.push(fn)
 
 const resolvers = {
   Query: {
     messages: () => messages,
   },
-  // Mutation: {
-  //   postMessage: (parent, { user, text }) => {
-  //     const id = messages.length
-  //     messages.push({ id, user, text })
-  //     return id
-  //   },
-  // },
+  Mutation: {
+    postMessage: (parent, { text, createdAt }) => {
+      const user = {
+        _id: 'uid',
+        name: 'Client One',
+        avatar: 'https://i.pravatar.cc/300',
+      }
+      const _id = messages.length + 1
+      messages.unshift({ _id, text, createdAt, user })
+      pubsub.publish('messages', { messages })
+      // subscribers.forEach((fn) => fn())
+      return _id
+    },
+  },
+  Subscription: {
+    messages: {
+      subscribe: () => pubsub.asyncIterator('messages'),
+    },
+  },
 }
+// Subscription: {
+//   messages: {
+//     subscribe: (parent, args, { pubsub }) => {
+//       const channel = Math.random().toString(36).substring(2, 15)
+//       onMessagesUpdates(() => pubsub.publish(channel, { messages }))
+//       setTimeout(() => pubsub.publish(channel, { messages }), 0)
+//       return pubsub.asyncIterator(channel)
+//     },
+//   },
+// },
 
-const server = createServer({
-  schema: {
+const startApolloServer = async () => {
+  const app = express()
+  const httpServer = createServer(app)
+
+  const schema = makeExecutableSchema({
     typeDefs,
     resolvers,
-  },
-})
-server.start()
+    context: { pubsub },
+  })
+
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: '/graphql',
+  })
+
+  const serverCleanup = useServer({ schema }, wsServer)
+
+  const server = new ApolloServer({
+    schema,
+    csrfPrevention: true,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose()
+            },
+          }
+        },
+      },
+    ],
+  })
+
+  await server.start()
+
+  server.applyMiddleware({ app, path: '/graphql', cors: true })
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve))
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  return { server, app }
+}
+
+startApolloServer()
